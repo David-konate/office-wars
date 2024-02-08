97bf65310bf913e01fa4898bc219c6eb8600351f
David-konate <56818495+David-konate@users.noreply.github.com>
2024-02-08 12:21:55 +0100

    route api implementer non testé

--- office-wars-api/app/Http/Controllers/API/UserController.php
+++ office-wars-api/app/Http/Controllers/API/UserController.php
@@ -21,74 +21,9 @@ class UserController extends Controller
         $users = User::all();
         return response()->json($users);
     }
-
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        try {
-            $validationUser = Validator::make($request->all(), [
-                'userFirstName' => 'required|min:1|string',
-                'userLastName' => 'required|min:1|string',
-                'city' => 'required|integer',
-                'userPseudo' => 'required|min:1|string|unique:users',
-                'mail' => 'required|min:1|string|unique:users',
-                'password' => 'required|string|min:8',
-                'phone' => 'nullable|string',
-                'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // Ajout de règles pour l'image
-            ]);
-
-            if ($validationUser->fails()) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Erreur de validation',
-                    'errors' => $validationUser->errors(),
-                ], 401);
-            }
-
-            $user = new User([
-                'userFirstName' => $request->userFirstName,
-                'userLastName' => $request->userLastName,
-                'city' => $request->city,
-                'userPseudo' => $request->userPseudo,
-                'mail' => $request->mail,
-                'password' => Hash::make($request->password),
-                'phone' => $request->phone,
-            ]);
-
-            // Vérifiez si un nouveau fichier image est téléchargé
-            if ($request->hasFile('userImage')) {
-                $filenameWithExt = $request->file('userImage')->getClientOriginalName();
-                $filenameWithExt = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-                $extension = $request->file('userImage')->getClientOriginalExtension();
-                $filename = $filenameWithExt . '_' . time() . '.' . $extension;
-                $request->file('userImage')->storeAs('public/uploads', $filename);
-
-                // Attribuez le nom du fichier à l'utilisateur
-                $user->image = $filename;
-            }
-
-            // Ajoutez le pseudo à l'utilisateur
-            $user->pseudo = $request->userPseudo;
-
-            // Sauvegardez l'utilisateur en base de données
-            $user->save();
-
-            return response()->json([
-                'data' => $user,
-                'status' => true,
-                'message' => 'Utilisateur créé avec succès'
-            ], 200);
-        } catch (\Throwable $e) {
-            return response()->json([
-                'status' => false,
-                'message' => 'Erreur lors de la création de l\'utilisateur : ' . $e->getMessage(),
-            ], 403);
-        }
-    }
-
-
     /**
      * Display the specified resource.
      */
@@ -121,10 +56,6 @@ class UserController extends Controller
             ], 500);
         }
     }
-
-
-
-
     /**
      * Update the specified resource in storage.
      */
@@ -190,8 +121,6 @@ class UserController extends Controller
             ], 403);
         }
     }
-
-
     /**
      * Remove the specified resource from storage.
      */
--- office-wars-api/routes/api.php
+++ office-wars-api/routes/api.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\AccomodationController;
+use App\Http\Controllers\Api\SecurityController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\PlanetController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\TypeController;
+use App\Models\Event;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +26,83 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//Route users
+Route::controller(SecurityController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+});
+
+//Route users
+Route::controller(UserController::class)->group(function () {
+    Route::get('users/{user}', 'show');
+    Route::get('users', 'index');
+    Route::post('users/{user}', 'update')->middleware('auth:api');
+    Route::delete('users/{user}', 'destroy')->middleware('auth:api');
+});
+
+//Route hotels
+Route::controller(AccomodationController::class)->group(function () {
+    Route::get('accommodations/{accommodation}', 'show');
+    Route::get('accommodations', 'index');
+    Route::post('accommodations', 'store')->middleware('auth:api');
+    Route::post('accommodations/{accommodation}', 'update')->middleware('auth:api');
+    Route::delete('accommodations/{accommodation}', 'destroy')->middleware('auth:api');
+});
+
+//Route évènements
+Route::controller(EventController::class)->group(function () {
+    Route::get('events/{event}', 'show');
+    Route::get('events', 'index');
+    Route::post('events', 'store')->middleware('auth:api');
+    Route::post('events/{event}', 'update')->middleware('auth:api');
+    Route::delete('events/{event}', 'destroy')->middleware('auth:api');
+});
+
+//Route images
+Route::controller(ImageController::class)->group(function () {
+    Route::get('images/{image}', 'show');
+    Route::get('images', 'index');
+    Route::post('images', 'store')->middleware('auth:api');
+    Route::post('images/{image}', 'update')->middleware('auth:api');
+    Route::delete('images/{image}', 'destroy')->middleware('auth:api');
+});
+
+//Route planetes
+Route::controller(PlanetController::class)->group(function () {
+    Route::get('planets/{planet}', 'show');
+    Route::get('planets', 'index');
+    Route::post('planets', 'store')->middleware('auth:api');
+    Route::post('planets/{planet}', 'update')->middleware('auth:api');
+    Route::delete('planets/{planet}', 'destroy')->middleware('auth:api');
+});
+
+
+//Route commentaires
+Route::controller(ReviewController::class)->group(function () {
+    Route::get('reviews/{review}', 'show');
+    Route::get('reviews', 'index');
+    Route::post('reviews', 'store')->middleware('auth:api');
+    Route::post('reviews/{review}', 'update')->middleware('auth:api');
+    Route::delete('reviews/{review}', 'destroy')->middleware('auth:api');
+});
+
+//Route des sites
+Route::controller(SiteController::class)->group(function () {
+    Route::get('sites/{site}', 'show');
+    Route::get('sites', 'index');
+    Route::post('sites', 'store')->middleware('auth:api');
+    Route::post('sites/{site}', 'update')->middleware('auth:api');
+    Route::delete('sites/{site}', 'destroy')->middleware('auth:api');
+});
+
+
+//Route des types
+Route::controller(TypeController::class)->group(function () {
+    Route::get('types/{type}', 'show');
+    Route::get('types', 'index');
+    Route::post('types', 'store')->middleware('auth:api');
+    Route::post('types/{type}', 'update')->middleware('auth:api');
+    Route::delete('types/{type}', 'destroy')->middleware('auth:api');
 });
